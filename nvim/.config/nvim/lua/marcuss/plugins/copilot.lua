90fae0af40ade2e80925f3f0282f3414b16fb6d9
Marcus Schimizzi <marcus@schimizzi.io>
2024-10-06 11:04:42 -0700

    update copilot configs to use other plugin

--- nvim/.config/nvim/lua/marcuss/plugins/copilot.lua
+++ nvim/.config/nvim/lua/marcuss/plugins/copilot.lua
@@ -1,3 +1,17 @@
 return {
-	"github/copilot.vim",
+	"zbirenbaum/copilot-cmp",
+	event = "InsertEnter",
+	config = function()
+		require("copilot_cmp").setup()
+	end,
+	dependencies = {
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
 }
