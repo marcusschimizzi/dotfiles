f9db4f86d7a4bc95a7d8dd9b7afa6171a9b5f853
Marcus Schimizzi <marcus@schimizzi.io>
2024-10-24 23:31:50 -0700

    add apple widget

--- sketchybar/.config/sketchybar/items/apple.lua
+++ sketchybar/.config/sketchybar/items/apple.lua
@@ -2,6 +2,8 @@ local colors = require("colors")
 local icons = require("icons")
 local settings = require("settings")
 
+sbar.add("item", { width = 5 })
+
 local apple = sbar.add("item", {
 	icon = {
 		font = { size = 16.0 },
@@ -11,8 +13,21 @@ local apple = sbar.add("item", {
 	},
 	label = { drawing = false },
 	background = {
-		color = colors.white,
+		color = colors.transparent,
+		border_color = colors.black,
+		border_width = 1,
 	},
 	padding_right = 1,
 	padding_left = 1,
+	click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 0",
 })
+
+sbar.add("bracket", { apple.name }, {
+	background = {
+		color = colors.transparent,
+		height = 30,
+		border_color = colors.grey,
+	},
+})
+
+sbar.add("item", { width = 7 })
