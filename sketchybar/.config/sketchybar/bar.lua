86848762d4f13edf5e53c489b0a7e68f25a0362c
Marcus Schimizzi <marcus@schimizzi.io>
2024-11-07 18:25:00 -0800

    minor bar styling changes

--- sketchybar/.config/sketchybar/bar.lua
+++ sketchybar/.config/sketchybar/bar.lua
@@ -3,11 +3,10 @@ local colors = require("colors")
 sbar.bar({
 	topmost = "window",
 	height = 40,
-	color = colors.bar.bg,
-	border_color = colors.bar.border,
+	color = colors.transparent,
 	blur_radius = 20,
-	sticky = true,
 	font_smoothing = true,
 	padding_left = 2,
 	padding_right = 2,
+	notch_width = 200,
 })
