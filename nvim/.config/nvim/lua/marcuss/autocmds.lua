4fcb21bf8b455aa5a48daacd35f94df6456d1898
Marcus Schimizzi <marcus@schimizzi.io>
2025-03-26 17:48:45 -0500

    a few other things for astro support

--- nvim/.config/nvim/lua/marcuss/autocmds.lua
+++ nvim/.config/nvim/lua/marcuss/autocmds.lua
@@ -21,3 +21,14 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHo
   command = "if mode() != 'c' | checktime | endif",
   desc = "Check if file changed externally",
 })
+
+-- Use markdown parser for mdx files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "mdx",
+  callback = function()
+    vim.defer_fn(function ()
+      vim.treesitter.language.register("markdown", "mdx")
+    end, 100)
+  end,
+  desc = "Use markdown Treesitter parser for MDX files"
+})
